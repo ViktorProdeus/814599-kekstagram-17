7c5a7f66e981344fe1a6e85825cb2317256b5723
Viktor <viktorprodeus@gmail.com>
2019-07-20 00:44:17 +0300

    Добавил модули

---
+++ js/gallery.js
@@ -0,0 +1,86 @@
+'use strict';
+
+(function () {
+  // Функция, возвращающая случайное число в диапазоне
+  function getRandomNumber(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  // Функция, возвращающая случайный элемемент массива
+  function getRandomElement(array) {
+    var randomIndex = Math.floor(Math.random() * array.length);
+    var randomElement = array[randomIndex];
+
+    return randomElement;
+  }
+
+  // Функция, которая создает массив комментариев
+  function createUserComments(messages, names) {
+    var userComments = [];
+    var count = getRandomNumber(0, window.dataUserPictures.COMMENTS_LENGTH);
+
+    for (var i = 1; i <= count; i++) {
+      var avatarCount = getRandomNumber(1, window.dataUserPictures.NUMBERS_AVATARS);
+
+      userComments.push({
+        avatars: 'img/avatar-' + avatarCount + '.svg',
+        messages: getRandomElement(messages),
+        names: getRandomElement(names)
+      });
+    }
+
+    return userComments;
+  }
+
+  // Функция, которая создает массив фотографий с данными
+  function createUserPictures() {
+    var userPictures = [];
+    var count = window.dataUserPictures.NUMBERS_IMAGES;
+
+    for (var i = 1; i <= count; i++) {
+      var LIKES_MIN = window.dataUserPictures.LIKES_MIN;
+      var LIKES_MAX = window.dataUserPictures.LIKES_MAX;
+      var MESSAGES = window.dataUserPictures.MESSAGES;
+      var NAMES = window.dataUserPictures.NAMES;
+
+      userPictures.push({
+        url: 'photos/' + i + '.jpg',
+        likes: getRandomNumber(LIKES_MIN, LIKES_MAX),
+        comments: createUserComments(MESSAGES, NAMES)
+      });
+    }
+
+    return userPictures;
+  }
+
+  // Генерируем шаблон фотографий
+  function renderUserPictures(picture) {
+    var picturesTemplate = document.querySelector('#picture')
+      .content.querySelector('.picture');
+
+    var cloneInElement = picturesTemplate.cloneNode(true);
+
+    cloneInElement.querySelector('.picture__img').src = picture.url;
+    cloneInElement.querySelector('.picture__likes').textContent = picture.likes;
+    cloneInElement.querySelector('.picture__comments').textContent = picture.comments.length;
+
+    return cloneInElement;
+  }
+
+  // Добавляем шаблон в контейнер для изображений
+  function addtoPictures(array) {
+    var fragment = document.createDocumentFragment();
+    var picturesContainer = document.querySelector('.pictures');
+
+    for (var i = 0; i < array.length; i++) {
+      picturesContainer.appendChild(renderUserPictures(array[i]));
+    }
+
+    return fragment;
+  }
+
+  // Выводим шаблон на страницу
+  addtoPictures(createUserPictures());
+
+})();

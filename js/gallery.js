806c2d2f150cb6cff70c47493fc06475f36a2036
Viktor <viktorprodeus@gmail.com>
2019-07-22 22:08:03 +0300

    внес изменение

--- js/gallery.js
+++ js/gallery.js
@@ -1,53 +1,6 @@
 'use strict';
 
 (function () {
-  // Функция, возвращающая случайное число в диапазоне
-  function getRandomNumber(min, max) {
-
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  // Функция, возвращающая случайный элемемент массива
-  function getRandomElement(array) {
-    var randomIndex = Math.floor(Math.random() * array.length);
-    var randomElement = array[randomIndex];
-
-    return randomElement;
-  }
-
-  // Функция, которая создает массив комментариев
-  function createUserComments(messages, names) {
-    var userComments = [];
-    var count = getRandomNumber(0, window.dataUserPictures.COMMENTS_LENGTH);
-
-    for (var i = 1; i <= count; i++) {
-      var avatarCount = getRandomNumber(1, window.dataUserPictures.NUMBERS_AVATARS);
-
-      userComments.push({
-        avatars: 'img/avatar-' + avatarCount + '.svg',
-        messages: getRandomElement(messages),
-        names: getRandomElement(names)
-      });
-    }
-
-    return userComments;
-  }
-
-  // Функция, которая создает массив фотографий с данными
-  function createUserPictures() {
-    var userPictures = [];
-    var count = window.dataUserPictures.NUMBERS_IMAGES;
-
-    for (var i = 1; i <= count; i++) {
-      userPictures.push({
-        url: 'photos/' + i + '.jpg',
-        likes: getRandomNumber(window.dataUserPictures.LIKES_MIN, window.dataUserPictures.LIKES_MAX),
-        comments: createUserComments(window.dataUserPictures.MESSAGES, window.dataUserPictures.NAMES)
-      });
-    }
-
-    return userPictures;
-  }
 
   // Генерируем шаблон фотографий
   function renderUserPictures(picture) {
@@ -64,7 +17,7 @@
   }
 
   // Добавляем шаблон в контейнер для изображений
-  function addtoPictures(array) {
+  var successHandler = function addtoPictures(array) {
     var fragment = document.createDocumentFragment();
     var picturesContainer = document.querySelector('.pictures');
 
@@ -73,9 +26,19 @@
     }
 
     return fragment;
-  }
+  };
 
-  // Выводим шаблон на страницу
-  addtoPictures(createUserPictures());
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; padding: 10px; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.load(successHandler, errorHandler);
 
 })();
